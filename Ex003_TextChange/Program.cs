7299a2007ae6e81270f4604742bd29f294a2ad98
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-04 15:36:59 +0300

    add and finish Ex003

--- Ex003_TextChange/Program.cs
+++ Ex003_TextChange/Program.cs
@@ -1,21 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+//заменить большими «К», а большие «С» заменить на маленькие «с».
+
+
 string text = "- Я думаю, — сказал князь, улыбаясь, - что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля."       + "Вы так красноречивы. Вы дадите мне чаю?";
 //string text = "Welcome to hell";
 
-//string Replace(string text, char oldValue, char newValue)
-//{
+string Replace(string text, char oldValue, char newValue)
+{
 string result = String.Empty;
 int length = text.Length;
-char oldValue = 'д';
-char newValue = 'Й';
 for (int i = 0; i < length; i++)
 {
     if(text[i] == oldValue) result = result + $"{newValue}";
     else  result = result + $"{text[i]}";
 }
-string newText = result;
-//return result;
-//}
-//string newText = Replace(text,"t","T");
+return result;
+}
+string newText = Replace(text,' ','|');
 Console.WriteLine(newText);
 
+newText = Replace(text,'к','К');
+Console.WriteLine(newText);
+
+newText = Replace(text,'С','с');
+Console.WriteLine(newText);
