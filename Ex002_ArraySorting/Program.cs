88802c8458ca5b1c5fa61c63d8d1094fa0aa8582
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-04 16:06:16 +0300

    add Ex002

--- Ex002_ArraySorting/Program.cs
+++ Ex002_ArraySorting/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: отсоротировать массив
+
+int[] arr = {1, 2, 3, 4, 5, 6, 7, 1 , 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i=0; i < count; i++ )
+    {
+        Console.Write($"{array[i]} ");
+    }
+Console.WriteLine();
+}
+Console.WriteLine();
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for(int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])  minPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+
+
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+
+PrintArray(arr);
+
+Console.WriteLine();
